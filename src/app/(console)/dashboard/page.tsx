08d8cfb49b70a5b3c7ffe3288000dade182f637c
Thant Hein Thwin <thantheinthwin.dev@gmail.com>
2024-05-03 22:09:52 +0700

    refactor: remove needless codes in dashboard page

--- src/app/(console)/dashboard/page.tsx
+++ src/app/(console)/dashboard/page.tsx
@@ -1,41 +1,6 @@
-"use client";
-
-// pages/index.tsx
-import { gql, useQuery } from "@apollo/client";
 import { ProjectsTable } from "./ProjectsTable";
 
-const GET_PROJECTS = gql`
-  query ListProjects {
-    listProjects {
-      items {
-        id
-        name
-        description
-      }
-    }
-  }
-`;
-
-interface Project {
-  id: string;
-  name: string;
-  description: string;
-}
-
-interface ProjectsData {
-  listProjects: {
-    items: Project[];
-  };
-}
-
 export default function HomePage() {
-  const { loading, error, data } = useQuery<ProjectsData>(GET_PROJECTS);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
-  const projects = data?.listProjects ? data?.listProjects.items : [];
-
   return (
     <div className="p-8">
       <ProjectsTable />
