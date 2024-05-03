7b151973f5b0f37852e51cd89fc531b1a8ac2879
Thant Hein Thwin <thantheinthwin.dev@gmail.com>
2024-05-03 23:46:33 +0700

    feat: React Query installed

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,12 +3,17 @@
 import { ApolloProvider } from "@apollo/client";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import client from "./apollo-client";
 
 export default function Providers({ children }: PropsWithChildren<{}>) {
+  const queryClient = new QueryClient();
+
   return (
     <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-      <ApolloProvider client={client}>{children}</ApolloProvider>
+      <QueryClientProvider client={queryClient}>
+        <ApolloProvider client={client}>{children}</ApolloProvider>
+      </QueryClientProvider>
     </NextThemesProvider>
   );
 }
